fd276cca5710a9f6c9b867d38343dd3f12a231bc
harpreet97 <harrycheema97@yahoo.com>
2017-09-04 14:05:39 +1200

    final commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace problem02csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //csharp problem02
+            
+            Console.WriteLine("This program will ask you to enter the number less than 100");
+            Console.WriteLine("************************************************************");
+            Console.WriteLine("Please type the number less than 100");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            if(number < 100){
+                for(int i = 1; i <= 12; i++){
+                    Console.WriteLine(number + "/" + i + "="+ (number/i));
+
+
+                }
+            }
+            else{
+                Console.WriteLine("Please type in the number less than 100");
+            }
+
         }
     }
 }
